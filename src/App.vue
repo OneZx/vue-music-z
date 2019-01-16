233a10e831788161ec82745d7bdd1fd86126512a
zxd <996029276@qq.com>
2019-01-16 12:50:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <m-header></m-header>
+        <tab></tab>
+        <router-view />
+    </div>
+</template>
+
+<script>
+import MHeader from './components/m-header/m-header'
+import Tab from 'components/tab/tab'
+export default {
+    name: 'App',
+    components: {
+        MHeader,
+        Tab
+    }
+}
+</script>
+
+<style scoped lang="stylus">
+// @import '～common/stylus/variable';
+@import '~common/stylus/variable';
+</style>
